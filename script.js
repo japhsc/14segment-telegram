2f9b739103cd26a7338374bf8d45a46d459d302f
friebetill <friebetill@gmail.com>
2023-12-30 02:29:23 +0100

    Add more security features for user

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   SegmentDisplayApp.init();
+
+  const textInput = document.getElementById("displayText");
+
+  textInput.addEventListener("input", () => {
+    const textLength = textInput.value.length;
+
+    if (textLength > 10) {
+      const rollRadioButton = document.querySelector(
+        'input[name="display_mode"][value="roll"]'
+      );
+      rollRadioButton.checked = true;
+    }
+  });
 });
 
 const SegmentDisplayApp = {
@@ -17,28 +30,37 @@ const SegmentDisplayApp = {
   },
 
   handleSendDataClick() {
-    const displayText = document.getElementById("displayText").value;
-    
-    if (!displayText) {
-        alert("Error: Text field cannot be empty!");
-        return;
+    let displayText = document.getElementById("displayText").value;
+
+    if (displayText[0] === "/") {
+      displayText = ` ${displayText}`;
     }
 
-    const effect = 'text';
+    if (!displayText) {
+      alert("Error: Text field cannot be empty!");
+      return;
+    }
+
+    const effect = "text";
 
     const color = document.getElementById("colorPicker").value;
     const redCommand = ` /red=${hexToInt(color.substring(1, 3))}`;
     const greenCommand = ` /green=${hexToInt(color.substring(3, 5))}`;
     const blueCommand = ` /blue=${hexToInt(color.substring(5, 7))}`;
 
-    const displayMode = document.querySelector('input[name="display_mode"]:checked').value;
-    const displayModeCommand = displayMode !== 'static' ? ` /${displayMode}` : "";
+    const displayMode = document.querySelector(
+      'input[name="display_mode"]:checked'
+    ).value;
+    const displayModeCommand =
+      displayMode !== "static" ? ` /${displayMode}` : "";
 
-    let countCommand = '';
-    if (displayMode === 'static') {
-      countCommand = ' /period=7000';
-    }else if (displayMode === 'roll' || displayMode === 'blink') {
-      countCommand = ' /count=5';
+    let countCommand = "";
+    if (displayMode === "static") {
+      countCommand = " /period=10000";
+    } else if (displayMode === "roll") {
+      countCommand = " /count=3";
+    } else if (displayMode === "blink") {
+      countCommand = " /count=10";
     }
 
     const displayTextCommand = ` ${displayText}`;
@@ -69,4 +91,3 @@ function hexToInt(hexString) {
 
   return intValue;
 }
-
